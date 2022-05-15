d1f8ae0f4f7737c5753c808f7d30f862c2feaa2c
Rutwik1 <83903631+Rutwik1@users.noreply.github.com>
2022-05-15 23:20:46 +0530

    loops (for, for in, for of, forEach)

---
+++ Day 3/task 1.js
@@ -0,0 +1,45 @@
+//For the given JSON iterate over all for loops (for, for in, for of, forEach).
+
+//for loop;-
+var obj = [
+{ person: "Rahul", age: "2", company: "GUVI" },
+{ person: "Rohit", age: "5", company: "GUVI geek" },
+{ person: "Rakesh", age: "8", company: "GUVI geek network" },
+]
+for(let i =0;i<obj.length;i++)
+{
+console.log(obj[i]);
+}
+
+
+//for of loop:-
+var obj = [
+{ person: "Rahul", age: "2", company: "GUVI" },
+{ person: "Rohit", age: "5", company: "GUVI geek" },
+{ person: "Rakesh", age: "8", company: "GUVI geek network" },
+]
+for(var property of obj)
+{
+console.log(property);
+}
+
+
+//for in loop:-
+var obj = [
+{ person: "Rahul", age: "2", company: "GUVI" },
+{ person: "Rohit", age: "5", company: "GUVI geek" },
+{ person: "Rakesh", age: "8", company: "GUVI geek network" },
+]
+for(var property in obj)
+{
+console.log(obj[property]);
+}
+
+
+//for each loop:-
+var obj = [
+{ person: "Rahul", age: "2", company: "GUVI" },
+{ person: "Rohit", age: "5", company: "GUVI geek" },
+{ person: "Rakesh", age: "8", company: "GUVI geek network" },
+]
+obj.forEach(key => console.log(key));
